66804450d119d7c6de2d6d324b0f179bb6313122
Michael Czekaj <145675402+czekem@users.noreply.github.com>
2024-02-08 09:33:18 +0100

    Update test.py

--- test.py
+++ test.py
@@ -24,3 +24,21 @@ def test_interpretation():
     player = 0
     computer = 2
     assert 'First player win!' in interpretation(player, computer)
+
+def test_interpretation_second():
+    player = 1
+    computer = 0
+    assert 'First player win!' in interpretation(player, computer)
+    
+
+
+def test_interpretation_third():
+    player = 0
+    computer = 1
+    assert 'Second player win!' in interpretation(player, computer)
+
+
+def test_interpretation_fourth():
+    player = 1
+    computer = 1
+    assert 'It\'s a draw' in interpretation(player, computer)
